e17c1cb4d30291a1785e993c0c6051f07b9375ee
Kranti19 <krantiks.vtalent@gmail.com>
2019-06-26 16:42:15 +0530

    KranthiDemo Controller Added in Controller Folder

---
+++ MVCGitExample/MVCGitExample/Controllers/KranthiDemoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCGitExample.Controllers
+{
+    public class KranthiDemoController : Controller
+    {
+        // GET: KranthiDemo
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
